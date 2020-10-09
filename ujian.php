a9b667064b1051e836c64b3f782ac685207f22d2
Matsnahr <70566403+Matsnahr@users.noreply.github.com>
2020-10-09 08:08:13 +0700

    alhamdulillah

---
+++ ujian.php
@@ -0,0 +1,11 @@
+<?php
+
+$merk = [
+    ["Tas", "Eiger", 1800000],
+    ["Tas", "Arei", 890000],
+    ["Tas", "Consina", 750000],
+  ["Jaket", "Arei ", 150000],
+    ["Jaket", "The North Face", 250000],
+    ["Sepatu", "Eiger", 1200000]
+    ];
+
